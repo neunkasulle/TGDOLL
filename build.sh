649c7290e8e4ad4b53ba8411d6b949b9643447f2
Jan <jan.zenkner@gmail.com>
2017-04-14 14:57:08 +0200

    [CI-CD] add proper exe name

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 rm -rf __pycache__ build/ dist/ main.spec
-pyinstaller -w -F -i "Icon-CYP-16x16.ico" main.py
+pyinstaller -n ChooseYourPlayer -w -F -i "Icon-CYP-16x16.ico" main.py
 cp -r res dist/res
 
 
